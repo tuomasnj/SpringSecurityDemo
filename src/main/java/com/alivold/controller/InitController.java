7de7932dded13f835a8361d00187c73f9886abfe
12554 <1255417198@qq,com>
2024-04-27 15:23:31 +0800

    引入springSecurity

---
+++ src/main/java/com/alivold/controller/InitController.java
@@ -0,0 +1,16 @@
+package com.alivold.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/init")
+public class InitController {
+    @GetMapping("/hello")
+    public String handleHello(){
+        return "Hello, SpringSecurity!";
+    }
+}
