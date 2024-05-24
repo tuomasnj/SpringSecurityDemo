4a8e250008dfebd78e018f7edfb31d0036be8eba
tuomasnj <1255407198@qq.com>
2024-05-24 16:09:02 +0800

    001

--- src/main/java/com/alivold/service/impl/LoginServiceImpl.java
+++ src/main/java/com/alivold/service/impl/LoginServiceImpl.java
@@ -13,13 +13,9 @@ import com.alivold.util.RedisCache;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-
-import javax.xml.ws.Response;
 
 @Service
 @Slf4j
